f2077d4525de17982651caddf4189fac40ea5499
pate5080 <133712406+pate5080@users.noreply.github.com>
2023-05-29 16:38:17 -0400

    Update MondayExample.java

--- In-Class Projects/MondayExample/src/mondayexample/MondayExample.java
+++ In-Class Projects/MondayExample/src/mondayexample/MondayExample.java
@@ -16,6 +16,7 @@ public class MondayExample {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println ("Learning github operations");
+        System.out.println ("Learning github operations pull");
     }
     
 }
